eb2d09db6f8fab5e1bb2711c4dd5a308d659bb41
pc <pc@pc.pc>
2015-10-12 18:03:24 +0200

    nuovo test

--- lol/src/lol/lol.java
+++ lol/src/lol/lol.java
@@ -1,9 +1,74 @@
 package lol;
 
 public class lol {
-	
+
 	public static void main(String[] args) {
-		System.out.println("Ciao di prova");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+		System.out.println("Ciao");
+
 	}
 
 }
